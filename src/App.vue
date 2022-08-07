21eb16010ffdbec46df3225ee462fcd8e6aad95d
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2022-08-07 15:36:46 +0100

    implemented header section

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,18 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@import url(https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&family=Roboto:wght@400;700&display=swap);
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,5 +1,25 @@
 <template>
   <header>
-     <h1>Bubble POP</h1>
+    <h1>Bubble POP</h1>
   </header>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
+
+header {
+  font-family: 'Righteous', serif;
+  background-color: #200070;
+  color: white;
+  width: 100%;
+  height: 8rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.26);
+  margin: 0;
+}
+</style>
