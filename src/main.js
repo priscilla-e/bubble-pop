899f4627cbb64ba37466e9c3b0cfea19e47ced6b
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2022-08-14 02:02:36 +0100

    added game spinner

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import store from './store/index.js';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseModal from './components/ui/BaseModal';
+import BaseSpiner from './components/ui/BaseSpiner';
 import App from './App.vue';
 
 const app = createApp(App);
@@ -10,6 +11,7 @@ const app = createApp(App);
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
 app.component('base-modal', BaseModal);
+app.component('base-spinner', BaseSpiner);
 
 app.use(store);
 
